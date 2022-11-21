b23aaae87d1d7bafae710bd5bc728b32d06764fe
LuluLiu66 <you@examplegit config --global user.name Your>
2022-11-21 21:49:43 +0800

    first version

---
+++ src/posts-mock-data.js
@@ -0,0 +1,12 @@
+export const postsMockData = [
+    {
+      id: "1",
+      title: "First-post",
+      content: "First-post-content",
+    },
+    {
+      id: "2",
+      title: "Second-post",
+      content: "Second-post-content",
+    },
+];
